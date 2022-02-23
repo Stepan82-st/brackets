cd1f528a59e1dd9f3bf9727b6a33d97bb6fdb287
Stepan Bryl <bryl.step@tut.by>
2022-02-23 12:53:16 +0300

    feat: index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackP = new Map();
+  const brackOpen = [];
+  const stack = [];
+
+  bracketsConfig.forEach(([elOpen, elClose]) => brackP.set(elClose, elOpen));
+  bracketsConfig.forEach(el => brackOpen.push(el[0]));
+
+  for (let i = 0; i < str.length; i++) {
+    let curChar = str[i];
+    if (brackOpen.includes(curChar) && !(brackP.get(curChar) === curChar && stack.includes(curChar) && stack[stack.length - 1] === curChar)) {
+      stack.push(curChar);
+    } else {
+      if (stack.length === 0) return false;
+      let uChar = stack[stack.length - 1];
+      if (uChar === brackP.get(curChar)) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0; // your solution
 }
